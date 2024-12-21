7b0d517f27257ef4649f14dda915b8e41d533d64
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-12-21 20:22:27 +0200

    Added Router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,47 +1,23 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner"
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from '../../resources/img/vision.png';
-
+import { MainPage, ComicsPage } from '../pages';
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null)
-
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
-
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                {/* <AppBanner></AppBanner> */}
-                {/* <ComicsList></ComicsList> */}
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Switch>
+                        <Route exact path="/">
 
-                <ErrorBoundary>
-                    <RandomChar />
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" />
-            </main>
-        </div>
+                        </Route>
+                        <Route exact path="/comics">
+
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router >
     )
 }
-
-
 export default App;
