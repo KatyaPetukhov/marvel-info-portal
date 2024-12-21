cde28c94ae0ec747bdcb0a70883ac466c3d8f221
Ekateryna Petukhov <83642262+KatyaPetukhov@users.noreply.github.com>
2024-12-21 19:03:17 +0200

    Added comics page component and services for it

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -33,6 +33,18 @@ const useMarvelService = () => {
 
     }
 
+    const getAllComics = async (offset = 0) => {
+        const res = await request(
+            `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+        );
+        return res.data.results.map(_transformComics);
+    };
+
+    const getComics = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
+    };
+
     const _transformCharacter = (character) => {
         return {
             id: character.id,
@@ -46,7 +58,26 @@ const useMarvelService = () => {
         }
 
     }
-    return { loading, error, getAllCharacters, getCharacter, clearError }
+
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || "There is no description",
+            pageCount: comics.pageCount
+                ? `${comics.pageCount} p.`
+                : "No information about the number of pages",
+            thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+            language: comics.textObjects[0]?.language || "en-us",
+            // optional chaining operator
+            price: comics.prices[0].price
+                ? `${comics.prices[0].price}$`
+                : "not available",
+        };
+    };
+
+
+    return { loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComics }
 
 }
 
